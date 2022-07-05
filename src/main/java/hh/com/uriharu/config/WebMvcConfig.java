6b4df3461ab060d95478a9b39048481ee3199d8e
heocat <bban777@gmail.com>
2022-07-05 21:35:13 +0900

    deploy 8

--- src/main/java/hh/com/uriharu/config/WebMvcConfig.java
+++ src/main/java/hh/com/uriharu/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins("http://localhost:3000")
+        .allowedOrigins("https://d2mx6dbqqs8vvm.cloudfront.net/")
         .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true)
